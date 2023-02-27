5f71c250c50f031d491f2dd8832cc2ecbcbd3725
Nicolas <nicolasdanielmalheiros@gmail.com>
2023-02-27 16:41:06 -0300

    Alteração

---
+++ Atv - Revisão/Ex005.c.c
@@ -0,0 +1,16 @@
+algoritmo "semnome"
+// Função :
+// Autor :
+// Data : 13/02/2023
+// Seção de Declarações
+var
+i : inteiro
+inicio
+i <- 100
+ENQUANTO i <> 199 FACA
+   i<- i + 1
+   SE (i mod 2 <> 0) ENTAO
+      escreval(i)
+   FIMSE
+FIMENQUANTO
+fimalgoritmo
